c835ec1f4c91fd2c411d75f9b6d70f88857057f2
Gufrana-04 <gufarana2004@gmail.com>
2026-04-30 15:09:05 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+#SYSTEM NAME
+SYSTEM_NAME="GatePass System"
+
+=======================================================
+REPLACE WITH YOUR CREDENTIALS
+=======================================================
+
+#DB CREDENTIALS
+# Oracle DB Credentials
+DB_USER = "your_db_username"
+DB_PASSWORD = "your_db_password"
+DB_DSN = "your_host:your_port/your_service_name"
+
+# Optional: Oracle Client Path
+ORACLE_CLIENT_PATH = r"C:\path\to\your\oracle\instantclient"
